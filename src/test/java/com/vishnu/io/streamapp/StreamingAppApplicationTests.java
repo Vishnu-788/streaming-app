70360b9b4da4f03eae7c97512fb2e7d877c00633
Vishnu Prasad J S <prasadvishnu788@gmail.com>
2026-07-09 20:27:19 +0530

    Initial Commit

---
+++ src/test/java/com/vishnu/io/streamapp/StreamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishnu.io.streamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
